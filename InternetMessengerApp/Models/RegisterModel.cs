ac68fd8110c08d872853d27fbd99c85500382ed6
MarcinBanas <Marrcin132@gmail.com>
2021-12-01 00:53:37 +0100

    Dodanie DisplayName w RegisterModel

--- InternetMessengerApp/Models/RegisterModel.cs
+++ InternetMessengerApp/Models/RegisterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace InternetMessengerApp.Models
     public class RegisterModel
     {
         public int UserId { get; set; }
+        [DisplayName("Nazwa użytkownika")]
         public string UserName { get; set; }
+        [DisplayName("Hasło")]
         public string UserPassword { get; set; }
+        
+        
     }
 }
