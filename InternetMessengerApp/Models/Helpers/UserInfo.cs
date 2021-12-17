af4446c0e8f1a71256407ed7c3cb6e0b7902d74c
Mikolaj Siwiec <siwiec.mikolaj@gmail.com>
2021-12-17 18:15:55 +0100

    helpery do logowania

---
+++ InternetMessengerApp/Models/Helpers/UserInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMessengerApp.Models.Helpers
+{
+    public class UserInfo
+    {
+        [Required(ErrorMessage = "Id is required")]
+        public int userId { get; set; }
+        [Required(ErrorMessage = "UserName is required")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+}
