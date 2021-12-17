e8533f26e2182581c1c7d9e369922a80451b9b00
Mikolaj Siwiec <siwiec.mikolaj@gmail.com>
2021-12-17 18:14:03 +0100

    Logika logowania/zapytanie do serwera/zwrot tokenu

---
+++ InternetMessengerApp/Models/Services/ServerAPIServices.cs
@@ -0,0 +1,40 @@
+﻿using InternetMessengerApp.Models.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace InternetMessengerApp.Models.Services
+{
+    public class ServerAPIServices : IDisposable
+    {
+        string baseUrl = "https://localhost:44369/"; //tymczasowe, tutaj idzie adres serwera
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetUserJWTToken(UserInfo userInfo)
+        {
+            string jsonUserString = JsonSerializer.Serialize(userInfo);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(jsonUserString);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri(baseUrl);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json'"));
+            HttpResponseMessage responseMessage = await client.PostAsync("/api/Token", byteContent);
+
+            string token = responseMessage.Content.ReadAsStringAsync().Result;
+
+            return token;
+        }
+    }
+}
---
+++ InternetMessengerApp/Models/Services/SignalRChatService.cs
@@ -0,0 +1,31 @@
+﻿
+using InternetMessengerApp.DomainModels;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace InternetMessengerApp.Services
+{
+    public class SignalRChatService
+    {
+        private readonly HubConnection _connection;
+
+        public event Action<Post> MessageReceived;
+        public SignalRChatService(HubConnection connection)
+        {
+            _connection = connection;
+
+            _connection.On<Post>("ReceiveMessage", (post) => MessageReceived?.Invoke(post));
+        }
+
+        public async Task Connect()
+        {
+            await _connection.StartAsync();
+        }
+
+        public async Task SendMessage(Post post)
+        {
+            await _connection.SendAsync("SendMessage", post);
+        }
+    }
+}
