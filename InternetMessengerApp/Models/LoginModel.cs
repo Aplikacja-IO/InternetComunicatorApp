1616094af5594df8e7e9a19034da1941b99bcb53
Mikolaj Siwiec <siwiec.mikolaj@gmail.com>
2021-12-17 18:12:37 +0100

    Model dla logowania

---
+++ InternetMessengerApp/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using InternetMessengerApp.Models.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMessengerApp.Models
+{
+    public class LoginModel
+    {
+        public UserInfo User{ get; set; }
+    }
+}
