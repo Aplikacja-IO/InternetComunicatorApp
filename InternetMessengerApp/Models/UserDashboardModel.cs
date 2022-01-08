7f7f6e72ee1996d15da455cea6aa4f79cc45a50b
Mikolaj Siwiec <siwiec.mikolaj@gmail.com>
2022-01-08 12:05:56 +0100

    userdashboard model z grupami usera

---
+++ InternetMessengerApp/Models/UserDashboardModel.cs
@@ -0,0 +1,27 @@
+﻿using InternetMessengerApp.DomainModels;
+using InternetMessengerApp.Models.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMessengerApp.Models
+{
+    public class UserDashboardModel
+    {
+        private int userId;
+        private string userToken;
+        public UserDashboardModel(int _userId, string _userToken)
+        {
+            userId = _userId;
+            userToken = _userToken;
+        }
+        public List<Group> MyGroups { get; set; }
+        public async Task GetAllUserGroups()
+        {
+            var server = new ServerAPIServices();
+            MyGroups = await server.GetAllUserGroupsAsync(userId, userToken);
+        }
+        public string test = "asasa";
+    }
+}
