b892364059fed16b95e984fe2e7ab38566633876
Mikolaj Siwiec <siwiec.mikolaj@gmail.com>
2022-01-16 11:15:06 +0100

    ChatModel - tworzenie polaczenia z hubem na serwerze

---
+++ InternetMessengerApp/Models/ChatModel.cs
@@ -0,0 +1,25 @@
+﻿using InternetMessengerApp.DomainModels;
+using Microsoft.AspNet.SignalR.Infrastructure;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMessengerApp.Models
+{
+    public class ChatModel
+    {
+        HubConnection connetion;
+        private void CreateConnectionForUserWithToken(RegisterUser user, string token)
+        {
+            Connection = new HubConnectionBuilder()
+                .WithUrl($"https://localhost:44369/ChatHub?userId={userId}", options =>
+                {
+                    options.AccessTokenProvider = () => Task.FromResult(tokenString);
+                })
+                .Build();
+            connectionId = connection.ConnectionId;
+        }
+    }
+}
