fcd8d012bc726e4f6fdefcac9a2c7a7ca2fdf382
Mikolaj Siwiec <siwiec.mikolaj@gmail.com>
2022-01-21 15:24:47 +0100

    ustawianie uzytkownika na zalogowanego

--- InternetMessengerApp/Models/MyProfileModel.cs
+++ InternetMessengerApp/Models/MyProfileModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using InternetMessengerApp.DomainModels;
+using InternetMessengerApp.Models.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,17 @@ namespace InternetMessengerApp.Models
 {
     public class MyProfileModel
     {
+        private string token;
+        private int userId;
+        public RegisterUser LoggedUser { get; set; }
+        public MyProfileModel(int _userId, string _token)
+        {
+            token = _token;
+        }
+        public async Task SetLoggedUser()
+        {
+            var server = new ServerAPIServices();
+            LoggedUser = await server.GetUserById(userId, token);
+        }
     }
 }
